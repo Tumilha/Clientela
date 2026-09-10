cc113439a97e08c37345cb30441dfd67996f92cc
Marky Ferreira <f.marky@escolar.ifrn.edu.br>
2026-09-09 21:23:08 -0300

    Fazendo as outras rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Inicial from "../componentes/inicial/inicial"
 import login from "../componentes/login/login"
+import admin from "../componentes/admin/admin"
+import caixa from "../componentes/caixa/caixa"
+import cadastro from "../componentes/cadastro/cadastro"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<inicial />} />
+      <Route path="/inicial" element={<inicial />} />
+      <Route path="/login" element={<login />} />
+      <Route path="/admin" element={<admin />} />
+      <Route path="/caixa" element={<caixa />} />
+      <Route path="/cadastro" element={<cadastro />} />
+
     </Routes>
   );
 }
